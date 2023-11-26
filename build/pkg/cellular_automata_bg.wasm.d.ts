f9521787de376dca75d0b6c67b853111b5b3e841
adamsaeid <adamsaeid@users.noreply.github.com>
2023-11-26 15:34:52 +0000

    deploy: a8d6a6a157c290befd0f89e344b0aa0804e57e47

--- build/pkg/cellular_automata_bg.wasm.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function elementary_ca(a: number, b: number, c: number): void;
-export function __wbindgen_free(a: number, b: number, c: number): void;
